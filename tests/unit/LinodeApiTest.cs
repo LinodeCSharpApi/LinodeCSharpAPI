c49b76afb502a5b6b61695385dda60477cf6e9a0
Juan Traverso <imobilis@gmail.com>
2013-10-26 15:34:49 +0200

    Skeleton for global library test.

---
+++ tests/unit/LinodeApiTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using JTraverso.LinodeCSharpAPI.Core;
+
+namespace JTraverso.LinodeCSharpAPI.tests.unit
+{
+    [TestFixture]
+    class LinodeApiTest
+    {
+        LinodeAPI apiObject;
+
+        [SetUp]
+        public void Init()
+        {
+            this.apiObject = new LinodeAPI();
+        }
+
+        [Test]
+        public void TestGet()
+        {
+            Response response = (Response)this.apiObject.Get("api.spec", new Dictionary<string, string>());
+        }
+    }
+}
